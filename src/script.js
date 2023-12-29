7f00942b78a2db246c4e3ea04ce20f269e06d02d
Sérgio Artifon <artifonn@gmail.com>
2023-12-29 10:54:16 -0300

    feat(javaScript): Implementa a busca e exibição de dados meteorológicos
    - Adicionado listener de evento para submissão do formulário, prevenindo o recarregamento padrão da página.
    - Implementada validação de entrada para verificar campo vazio.
    - Construída chamada à API do OpenWeatherMap para buscar dados meteorológicos.
    - Adicionado tratamento de erro para respostas não bem-sucedidas da API.
    - Implementada manipulação do DOM para exibir informações sobre o clima, incluindo temperatura, velocidade do vento e indicador de direção.
    - Corrigido pequeno erro de digitação na construção da string HTML para exibição das informações do vento.
    - Garantida a manipulação assíncrona das requisições à API usando async/await.
    - Melhorado o feedback ao usuário com mensagens de carregamento e erro.

---
+++ src/script.js
@@ -0,0 +1,54 @@
+document.querySelector('.buscar').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  let input = document.querySelector('.input').value;
+
+  if (input !== '') {
+    clearInfo();
+    mensagem('Carregando...');
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=b8e0deec2d79d9c716797a96184a2f3a&units=metric&lang=pt_br`;
+
+    let req = await fetch(url);
+    let res = await req.json();
+
+    if (res.cod === 200) {
+      resultInfo({
+        name: res.name,
+        pais: res.sys.country,
+        temp: res.main.temp,
+        tempIcon: res.weather[0].icon,
+        windSpeed: res.wind.speed,
+        windDeg: res.wind.deg,
+      })
+    } else {
+      clearInfo();
+      mensagem('Localização não encontrada. Tente novamente!');
+    }
+  } else {
+    clearInfo();
+  }
+});
+
+function resultInfo(res) {
+  mensagem('');
+
+  document.querySelector('.titulo').innerHTML = `${res.name}, ${res.pais}`;
+  document.querySelector('.tempInfo').innerHTML = `${res.temp}<sup>°C</sup>`;
+  document.querySelector('.ventoInfo').innerHTML = `${res.windSpeed}<span>Km/h</span>`;
+
+  document.querySelector('.temp img').setAttribute('src', `https://openweathermap.org/img/wn/${res.tempIcon}@2x.png`);
+
+  document.querySelector('.ventoPonto').style.transform = `rotate(${res.windDeg - 90}deg)`;
+
+  document.querySelector('.result').style.display = 'block';
+}
+
+function mensagem(msg) {
+  document.querySelector('.aviso').innerHTML = msg;
+}
+
+function clearInfo() {
+  mensagem('');
+  document.querySelector('.result').style.display = 'none';
+}
